3ad61329ac936f98cadf5f27bb7713bdbd10e00a
Nkebenyane <42379845+Nkebenyane@users.noreply.github.com>
2021-03-25 13:59:08 +0200

    download  pdf

--- js/downloadpdf.js
+++ js/downloadpdf.js
@@ -1,20 +1,13 @@
-function getPDF() {
+function genPDF(){
     var doc = new jsPDF();
-   
-    // We'll make our own renderer to skip this editor
+
     var specialElementHandlers = {
-      '#DontPrintPdf': function(element, renderer){
-        return true;
-      }
-     
+        '#DontPrintPdf' : function(element, render) {return true;}
     };
-  
-    // All units are in the set measurement for the document
-    // This can be changed to "pt" (points), "mm" (Default), "cm", "in"
-    doc.fromHTML($('#body').get(0), 15, 15, {
-      'width': 170, 
-      'elementHandlers': specialElementHandlers
+
+    doc.fromHTML($('#body').get(0),20,20,{
+        'width': 500,
+        'elementHandlers': specialElementHandlers
     });
-  
-    doc.save('Generated.pdf');
-  }
+    doc.save('Maipato Nkebenyane.pdf')
+}
