168e94ab8ebde0e9aacbeccb9c5a231d2c59fd53
Nkebenyane <42379845+Nkebenyane@users.noreply.github.com>
2021-03-25 16:52:33 +0200

    download  pdf

--- js/downloadpdf.js
+++ js/downloadpdf.js
@@ -1,13 +1,28 @@
-function genPDF(){
-    var doc = new jsPDF();
+// function genPDF(){
+//     var doc = new jsPDF();
 
-    var specialElementHandlers = {
-        '#DontPrintPdf' : function(element, render) {return true;}
-    };
+//     var specialElementHandlers = {
+//         '#DontPrintPdf' : function(element, render) {return true;}
+//     };
 
-    doc.fromHTML($('#body').get(0),20,20,{
-        'width': 500,
-        'elementHandlers': specialElementHandlers
+//     doc.fromHTML($('#body').get(0),20,20,{
+//         'width': 500,
+//         'elementHandlers': specialElementHandlers
+//     });
+//     doc.save('Maipato Nkebenyane.pdf')
+// }
+
+var doc = new jsPDF();
+var specialElementHandlers = {
+    '#DontPrintPdf': function (element, renderer) {
+        return true;
+    }
+};
+
+$('#cmd').click(function () {   
+    doc.fromHTML($('#content').html(), 15, 15, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
     });
-    doc.save('Maipato Nkebenyane.pdf')
-}
+    doc.save('Maipato Nkebenyane CV.pdf');
+});
