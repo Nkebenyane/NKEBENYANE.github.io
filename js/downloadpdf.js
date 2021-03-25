3faab50818c0c5f8927b01e46e20e716e582f20a
Nkebenyane <42379845+Nkebenyane@users.noreply.github.com>
2021-03-25 18:10:08 +0200

    download  pdf

--- js/downloadpdf.js
+++ js/downloadpdf.js
@@ -8,25 +8,8 @@ function genPDF() {
     doc.fromHTML($('#body').get(0),20,20,{
         'width': 500,
         'elementHandlers': specialElementHandlers
-    });
+    }, function cb() {});
     doc.save('Maipato Nkebenyane.pdf')
 
     console.log('hello i am trying to print')
 }
-
-var doc = new jsPDF();
-var specialElementHandlers = {
-    '#DontPrintPdf': function (element, renderer) {
-        return true;
-    }
-};
-
-$('#cmd').click(function () {
-
-
-    doc.fromHTML($('#body').html(), 15, 15, {
-        'width': 170,
-        'elementHandlers': specialElementHandlers
-    });
-    doc.save('Maipato Nkebenyane CV.pdf');
-});
