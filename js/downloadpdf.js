a241bbc34b1e4c24cb09adf7cc8c4727ad71528d
Nkebenyane <42379845+Nkebenyane@users.noreply.github.com>
2021-03-25 12:27:23 +0200

    download not working pdf

--- js/downloadpdf.js
+++ js/downloadpdf.js
@@ -2,14 +2,15 @@ function genPDF(){
     var doc = new jsPDF();
 
     var specialElementHandlers = {
-        '#DontPrintPdf' : function(element, render) {return true}
+        '#DontPrintPdf' : function(element, render) {return true;}
     };
 
     doc.fromHTML($('#body').get(0),20,20,{
         'width': 500,
         'elementHandlers': specialElementHandlers
+        
     });
-    setTimeout(function () {
-        doc.save('Maipato Nkebenyane.pdf');
-    }, 0)
+    
+    doc.save('Maipato Nkebenyane.pdf');
+    
 }
