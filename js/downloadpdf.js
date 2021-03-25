d49d2e8528d57f8025ceb054d7ee3f5d4b168cf9
Nkebenyane <42379845+Nkebenyane@users.noreply.github.com>
2021-03-25 11:47:38 +0200

    download pdf

---
+++ js/downloadpdf.js
@@ -0,0 +1,13 @@
+function genPDF(){
+    var doc = new jsPDF();
+
+    var specialElementHandlers = {
+        '#DontPrintPdf' : function(element, render) {return true}
+    };
+
+    doc.fromHTML($('#body').get(0),20,20,{
+        'width': 500,
+        'elementHandlers': specialElementHandlers
+    });
+    doc.save('Maipato Nkebenyane.pdf');
+}
