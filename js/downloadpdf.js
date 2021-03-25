439a843ed5b5e46d957fa46a7c2d4a9a912540a5
Nkebenyane <42379845+Nkebenyane@users.noreply.github.com>
2021-03-25 17:01:40 +0200

    download  pdf

--- js/downloadpdf.js
+++ js/downloadpdf.js
@@ -1,28 +1,30 @@
-// function genPDF(){
-//     var doc = new jsPDF();
+function genPDF() {
+    //     var doc = new jsPDF();
 
-//     var specialElementHandlers = {
-//         '#DontPrintPdf' : function(element, render) {return true;}
-//     };
+    //     var specialElementHandlers = {
+    //         '#DontPrintPdf' : function(element, render) {return true;}
+    //     };
 
-//     doc.fromHTML($('#body').get(0),20,20,{
-//         'width': 500,
-//         'elementHandlers': specialElementHandlers
-//     });
-//     doc.save('Maipato Nkebenyane.pdf')
-// }
+    //     doc.fromHTML($('#body').get(0),20,20,{
+    //         'width': 500,
+    //         'elementHandlers': specialElementHandlers
+    //     });
+    //     doc.save('Maipato Nkebenyane.pdf')
+    console.log('hello i am trying to print')
 
-var doc = new jsPDF();
-var specialElementHandlers = {
-    '#DontPrintPdf': function (element, renderer) {
-        return true;
-    }
-};
 
-$('#cmd').click(function () {   
-    doc.fromHTML($('#content').html(), 15, 15, {
-        'width': 170,
+    var doc = new jsPDF();
+    var specialElementHandlers = {
+        '#DontPrintPdf': function (element, renderer) {
+            return true;
+        }
+    };
+
+    $('#cmd').click(function () {
+        doc.fromHTML($('#body').html(), 15, 15, {
+            'width': 170,
             'elementHandlers': specialElementHandlers
+        });
+        doc.save('Maipato Nkebenyane CV.pdf');
     });
-    doc.save('Maipato Nkebenyane CV.pdf');
-});
+}
